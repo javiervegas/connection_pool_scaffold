f630b0e9b7bfebe1fe088161a341e3c759e53b86
Javier Vegas <javiervegas@Javier-Vegass-MacBook-Pro.local>
2011-05-15 20:46:21 -0700

    simple pool of fixed queue, backed by a queue

--- src/main/scala/org/javiervegas/connectionpool/SimpleConnectionPool.scala
+++ src/main/scala/org/javiervegas/connectionpool/SimpleConnectionPool.scala
@@ -2,14 +2,22 @@ package com.opower.connectionpool
 import java.sql.Connection
 import java.sql.SQLException
 import javax.sql.DataSource
+import scala.collection.mutable.Queue
 
-class SimpleConnectionPool(val ds: DataSource) extends ConnectionPool {
+class SimpleConnectionPool(val ds: DataSource, val size: Int = 10) extends ConnectionPool {
+
+  private val availableConnectionPool = new Queue[Connection]
+  availableConnectionPool ++= (1 to size) map {_ => ds.getConnection}
 
   def getConnection(): Connection = {
-    ds.getConnection
+    try {
+      availableConnectionPool.dequeue
+    } catch {
+      case _ => throw new SQLException
+    }
   }
 
   def releaseConnection(connection: Connection): Unit = {
-    //ignore
+    availableConnectionPool += connection
   }
 }
--- src/test/scala/org/javiervegas/connectionpool/ConnectionPoolSpec.scala
+++ src/test/scala/org/javiervegas/connectionpool/ConnectionPoolSpec.scala
@@ -22,29 +22,82 @@ class ConnectionPoolSpec extends FeatureSpec with GivenWhenThen with MustMatcher
     info("I want to be able to get and return connections from a pool")
     info("So that I can reuse open connections")
  
-    def getMockDataSource():DataSource = {
+    def getMockDataSource() = {
         val ds = mock[DataSource]
         ds.getConnection returns mock[Connection]
         ds
     }
 
-    def defaultConnectionPool():ConnectionPool = {
-        new SimpleConnectionPool(getMockDataSource)
-    }
-
-    scenario("it gives out and accepts back connections") {
+    scenario("a connection pool gives out and accepts back connections") {
 
       given("a connection pool")
-      val cp = defaultConnectionPool
+      val cp = new SimpleConnectionPool(getMockDataSource)
 
       when("when I borrow a connection")
       val conn = cp.getConnection
 
       then("it will not be null")
       conn must not be (null)
-      then("and I can give it back")
+      and("and I can give it back")
       cp.releaseConnection(conn)
     }
+
+    scenario("a default pool must have 10 connections") {
+
+      given("a default connection pool")
+      val ds = getMockDataSource
+      val cp = new SimpleConnectionPool(ds)
+
+      then("ten connections were created")
+      there was 10.times(ds).getConnection
+    }                                                                              
+
+    scenario("a new pool must be full of connections") {
+
+      given("a initial pool size")
+      val size = 100
+
+      when("when I create a connection pool of that size")
+      val ds = getMockDataSource
+      val cp = new SimpleConnectionPool(ds, size)
+
+      then("that many connections were created")
+      there was size.times(ds).getConnection
+    }
+
+    scenario("a pool will error out when I ask for more connections than its size") {
+
+      given("a connection pool of a given size")
+      val size = 20
+      val ds = getMockDataSource
+      val cp = new SimpleConnectionPool(ds, size)
+
+      when("when I ask for as many connections as its size")
+      1 to size foreach { _ => cp.getConnection }
+      and("I ask for another connection")
+    
+      then("SQLException should be thrown")
+      evaluating { cp.getConnection() } must produce [SQLException]
+    }
+
+    scenario("a pool will make returned connections available for reuse") {
+
+      given("a connection pool of a given size")
+      val size = 8
+      val ds = getMockDataSource
+      val cp = new SimpleConnectionPool(ds, size)
+
+      when("when I retrieve the max number of connections")
+      val conns = 1 to size map { _ => cp.getConnection }
+      and("I return all of them")
+      conns foreach { conn => cp.releaseConnection(conn) }
+    
+      then("I can borrow them again")
+      1 to size foreach { _ => cp.getConnection }
+      then("and no new connections were created")
+      there was size.times(ds).getConnection
+    }
+
   }
 }
 
