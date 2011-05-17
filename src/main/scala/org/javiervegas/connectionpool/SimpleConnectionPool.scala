ddf646fa81024d577d39ca7fdd8da83b0ba298a9
Javier Vegas <javiervegas@Javier-Vegass-MacBook-Pro.local>
2011-05-17 01:50:34 -0700

    use Driver instead of DataSource to as param to construct ConnectionPool

--- src/main/scala/org/javiervegas/connectionpool/SimpleConnectionPool.scala
+++ src/main/scala/org/javiervegas/connectionpool/SimpleConnectionPool.scala
@@ -3,17 +3,21 @@ package org.javiervegas.connectionpool
 import com.opower.connectionpool.ConnectionPool
 import java.sql.Connection
 import java.sql.SQLException
+import java.sql.Driver
 import java.sql.DriverManager
 import javax.sql.DataSource
 import scala.collection.mutable.Queue
 
-class SimpleConnectionPool(val url: String , val userName: String, val password: String, driver: String, val size: Int = 10) extends ConnectionPool {
-
-  Class.forName(driver).newInstance()
+class SimpleConnectionPool(val url: String , val userName: String, val password: String, driver: Driver, val size: Int = 10) extends ConnectionPool {
+  DriverManager.registerDriver(driver)
 
   private val availableConnectionPool = new Queue[Connection]
   (1 to size) foreach {_ => 
     availableConnectionPool.enqueue(new PooledConnection(createConnection, this))
+  }
+
+  def this(url: String, userName: String, password: String, driverName: String, size: Int) {
+    this(url, userName, password, Class.forName(driverName).newInstance.asInstanceOf[Driver], size)
   }
 
   private def createConnection = {
@@ -30,7 +34,7 @@ class SimpleConnectionPool(val url: String , val userName: String, val password:
 
   def releaseConnection(conn: Connection): Unit = {
     conn match {
-      case conn: PooledConnection => availableConnectionPool.enqueue(conn)
+      case conn: PooledConnection if conn.cp == this => availableConnectionPool.enqueue(conn)
       case _ => throw new RuntimeException
     }
   }
--- src/test/scala/org/javiervegas/connectionpool/ConnectionPoolSpec.scala
+++ src/test/scala/org/javiervegas/connectionpool/ConnectionPoolSpec.scala
@@ -1,9 +1,10 @@
-package com.opower.connectionpool.specification
+package org.javiervegas.connectionpool.specification
 
-import com.opower.connectionpool._
+import org.javiervegas.connectionpool._
 import java.sql.Connection
-import javax.sql.DataSource
+import java.sql.Driver
 import java.sql.SQLException
+import java.util.Properties
 import org.junit.runner.RunWith
 import org.scalatest.FeatureSpec
 import org.scalatest.GivenWhenThen
@@ -22,16 +23,23 @@ class ConnectionPoolSpec extends FeatureSpec with GivenWhenThen with MustMatcher
     info("I want to be able to get and return connections from a pool")
     info("So that I can reuse open connections")
  
-    def getMockDataSource() = {
-        val ds = mock[DataSource]
-        ds.getConnection returns mock[Connection]
-        ds
+    def getMockDriver() = {
+        val md = mock[Driver]
+        md.connect(any[String], any[Properties]) returns mock[Connection]
+        md
+    }
+   
+    def getConnectionPool(driver: Driver = getMockDriver, size: Option[Int] = None) = {
+        size match {
+            case None => new SimpleConnectionPool("dummyUrl","dummyUserName","dummyPassword", driver)
+            case Some(s) => new SimpleConnectionPool("dummyUrl","dummyUserName","dummyPassword", driver, s)
+        }
     }
 
     scenario("a connection pool gives out and accepts back connections") {
 
       given("a connection pool")
-      val cp = new SimpleConnectionPool(getMockDataSource)
+      val cp = getConnectionPool()
 
       when("when I borrow a connection")
       val conn = cp.getConnection
@@ -45,11 +53,11 @@ class ConnectionPoolSpec extends FeatureSpec with GivenWhenThen with MustMatcher
     scenario("a default pool must have 10 connections") {
 
       given("a default connection pool")
-      val ds = getMockDataSource
-      val cp = new SimpleConnectionPool(ds)
+      val dr = getMockDriver
+      val cp = getConnectionPool(dr)
 
       then("ten connections were created")
-      there was 10.times(ds).getConnection
+      there was 10.times(dr).connect(_: String, _: Properties)
     }                                                                              
 
     scenario("a new pool must be full of connections") {
@@ -58,18 +66,18 @@ class ConnectionPoolSpec extends FeatureSpec with GivenWhenThen with MustMatcher
       val size = 100
 
       when("when I create a connection pool of that size")
-      val ds = getMockDataSource
-      val cp = new SimpleConnectionPool(ds, size)
+      val dr = getMockDriver
+      val cp = getConnectionPool(dr, Some(size))
 
       then("that many connections were created")
-      there was size.times(ds).getConnection
+      there was size.times(dr).connect(_: String, _: Properties)
     }
 
     scenario("a pool will error out when I ask for more connections than its size") {
 
       given("a connection pool of a given size")
       val size = 20
-      val cp = new SimpleConnectionPool(getMockDataSource, size)
+      val cp = getConnectionPool(getMockDriver, Some(size))
 
       when("when I ask for as many connections as its size")
       1 to size foreach { _ => cp.getConnection }
@@ -79,12 +87,12 @@ class ConnectionPoolSpec extends FeatureSpec with GivenWhenThen with MustMatcher
       evaluating { cp.getConnection() } must produce [SQLException]
     }
 
-    scenario("a pool will make returned connections available for reuse") {
+    scenario("a pool will make released connections available for reuse") {
 
       given("a connection pool of a given size")
       val size = 8
-      val ds = getMockDataSource
-      val cp = new SimpleConnectionPool(ds, size)
+      val driver = getMockDriver
+      val cp = getConnectionPool(driver, Some(size))
 
       when("when I retrieve the max number of connections")
       val conns = 1 to size map { _ => cp.getConnection }
@@ -94,12 +102,12 @@ class ConnectionPoolSpec extends FeatureSpec with GivenWhenThen with MustMatcher
       then("I can borrow them again")
       1 to size foreach { _ => cp.getConnection }
       then("and no new connections were created")
-      there was size.times(ds).getConnection
+      there was size.times(driver).connect(_: String, _: Properties)
     }
 
     scenario("a pool will only accept returns of pooled connections") {
       given("a connection pool")
-      val cp = new SimpleConnectionPool(getMockDataSource)
+      val cp = getConnectionPool()
 
       when("when I return a generic non-pooled connection")
       val genericConn = mock[Connection]
@@ -110,10 +118,10 @@ class ConnectionPoolSpec extends FeatureSpec with GivenWhenThen with MustMatcher
 
     scenario("a pool will not accept returns of connections created by another pool") {
       given("a connection pool")
-      val cp = new SimpleConnectionPool(getMockDataSource)
+      val cp = getConnectionPool()
 
       when("when I return connections created by another pool")
-      val otherPoolConn = (new SimpleConnectionPool(getMockDataSource)).getConnection
+      val otherPoolConn = getConnectionPool().getConnection
     
       then("Exceptions should be thrown")
       evaluating { cp.releaseConnection(otherPoolConn) } must produce [Exception]
